30f6dd558af9bfd5e16f1538db08bcef348f826e
nizhantiw <nizhantiw@gmail.com>
2022-12-27 21:27:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
 import React,{useState,useEffect,useRef} from 'react'
 import '../styles/App.css';
 const App = () => {
-  state={
-  email: "",
-  password: ""
-  }
-  handleSubmit = e => {
-    e.preventDefault();
-    this.setState({ email: this.refs.textInput.email, password: this.refs.textInput.password })
-  };
 
-//code here 
+//code here
+  const [value2, SetValue2] = useState("");
+  const [value, SetValue] = useState("");
+  const newRef = useRef(null);
+
+  const settingValue = () => {
+    SetValue2(value);
+    SetValue("");
+  }
+
+  const handle = () => { }
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={this.state.email} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={value} ref={newRef} onChange={}/><br/>
       Password
-      <input id="inputPassword" type="text" value={this.state.password} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
+      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
+      <button id="submitButton" onClick={handle}>Submit</button><br/>
       <p id="emailText">Your Email : {}</p>
       <p id ="passwordText">Your Password : {}</p>
       
